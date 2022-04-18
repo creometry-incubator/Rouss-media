545b9896e471e54cbb7fa06ed3bf3391e9bb6f60
hadhemi-ouni <ounihadhami@gmail.com>
2022-04-18 22:18:19 +0100

    designing categories buttons
    Signed-off-by: hadhemi-ouni <ounihadhami@gmail.com>

---
+++ index/public/config.js
@@ -0,0 +1,5 @@
+window.ENV = {
+    "ARTICLE_SERVICE_URI": "http://localhost:5000/article",
+    "AUTHOR_SERVICE_URI": "http://localhost:5000/author",
+    "GOOGLE_CLIENT_ID": "866999069656-mjqq3p4c1amrm6kgcnn76i3kna4v87fc.apps.googleusercontent.com"
+}
