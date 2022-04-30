98bd105e515ef351af91e59249ce57828664feb2
unknown <azizbenhadjyahia99@gmail.com>
2022-04-30 17:05:59 +0000

    last touch

--- index/src/components/Sidebar.js
+++ index/src/components/Sidebar.js
@@ -29,20 +29,6 @@ class Sidebar extends Component {
                                     </div>
                                 </div>
                             </div>
-                            <div className="widget">
-                                <h2 className="widget-title">Popular Categories</h2>
-                                <div className="link-widget">
-                                    <ul>
-                                        <li><a href="#">Fahsion <span>(21)</span></a></li>
-                                        <li><a href="#">Lifestyle <span>(15)</span></a></li>
-                                        <li><a href="#">Art & Design <span>(31)</span></a></li>
-                                        <li><a href="#">Health Beauty <span>(22)</span></a></li>
-                                        <li><a href="#">Clothing <span>(66)</span></a></li>
-                                        <li><a href="#">Entertaintment <span>(11)</span></a></li>
-                                        <li><a href="#">Food & Drink <span>(87)</span></a></li>
-                                    </ul>
-                                </div>
-                            </div>
                         </div>
         );
     }
